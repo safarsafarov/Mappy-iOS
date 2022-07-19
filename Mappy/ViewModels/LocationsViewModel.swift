59b0b0a37d1f618501ccfb876661994bbd722452
Safar Safarov <Safarov.safar@hotmail.com>
2022-07-19 14:35:32 +0500

    ✨

--- Mappy/ViewModels/LocationsViewModel.swift
+++ Mappy/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,29 @@
 //
 
 import Foundation
+import MapKit
 
 class LocationsViewModel: ObservableObject {
+    
+    // All loaded locations
     @Published var locations: [Location]
+    
+    //Current location on map
+    @Published var mapLocation: Location
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocations = locations.first!
+    }
+    
+    private func updateMapRegion(location: Location) {
+        mapRegion = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan
+        )
     }
 }
