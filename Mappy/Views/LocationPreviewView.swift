307709f7b6aa0b751fb645329989bbcd8c4ce7ec
Safar Safarov <Safarov.safar@hotmail.com>
2022-07-23 18:22:42 +0500

    ✨

---
+++ Mappy/Views/LocationPreviewView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationPreviewView.swift
+//  Mappy
+//
+//  Created by Safar Safarov on 23/07/22.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationPreviewView()
+    }
+}
