6d87056fc4d2dba711f91feb278d478ec9b00cf0
Safar Safarov <Safarov.safar@hotmail.com>
2022-07-25 15:33:52 +0500

    ✨

--- Mappy/Views/LocationPreviewView.swift
+++ Mappy/Views/LocationPreviewView.swift
@@ -8,22 +8,80 @@
 import SwiftUI
 
 struct LocationPreviewView: View {
+    let location: Location
     var body: some View {
-        VStack(spacing: 16) {
-            
-            ZStack {
-                if let imageName = location.imageNames.first {
-                    Image(imageName)
-                }
+        HStack {
+            VStack(spacing: 16) {
+                imageSection
+                titleSection
             }
             
-            Text(location.name)
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+            }
         }
+       
     }
 }
 
 struct LocationPreviewView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationPreviewView(location: LocationsDataService.locations.first!)
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            LocationPreviewView(location: LocationsDataService.locations.first!)
+        }
+      
+    }
+}
+
+extension LocationPreviewView {
+    private var imageSection: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+        
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {                Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            Text(location.cityName)
+                .font(.subheadline)
+            
+                
+        }
+    }
+    
+    private var learnMoreButton: some View {
+        Button {
+            
+        } label: {
+            Text("Learn more")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button {
+            
+        } label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
     }
 }
