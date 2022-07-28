491b1b01d853f25e2141e618cfd44b1859c622dc
Safar Safarov <Safarov.safar@hotmail.com>
2022-07-28 14:35:39 +0500

    1

---
+++ Mappy/Views/LocationDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  LocationDetailView.swift
+//  Mappy
+//
+//  Created by Safar Safarov on 28/07/22.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    
+    let location: Location
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                imageSection
+                
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView(location: LocationsDataService.locations.first!)
+    }
+}
+
+
+extension LocationDetailView {
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
